2a4f630ffe2c35bf41e27f57e527c22a14c7e689
flyeagle12 <117089784+flyeagle12@users.noreply.github.com>
2024-09-14 21:36:49 +0300

    Create playground..js

---
+++ playground..js
@@ -0,0 +1,6 @@
+const stations = {
+  num1: {a: {clicked: false, color: 'gray'}, b: {clicked: false, color: 'gray'}, 'a+b': {clicked: false, color: 'gray'}},
+  num2: {a: {clicked: false, color: 'gray'}, b: {clicked: false, color: 'gray'}, 'a+b': {clicked: false, color: 'gray'}}
+}
+
+console.log(stations[a])
